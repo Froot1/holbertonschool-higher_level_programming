ecc367690c5768e1d6cd0e6aa7904d1ca9fafe53
root <10679@holbertonstudents.com>
2025-06-03 15:09:49 +0000

    10-student.py

--- python-input_output/10-student.py
+++ python-input_output/10-student.py
@@ -1,19 +1,19 @@
 #!/usr/bin/python3
-"""student class module"""
+"""Student class module."""
 
 
 class Student:
-    """a class we shall json"""
+    """A class to be jsonified."""
+
     def __init__(self, first_name, last_name, age):
-        """init method"""
+        """Initialize a Student."""
         self.first_name = first_name
         self.last_name = last_name
         self.age = age
 
     def to_json(self, attrs=None):
-        """get dict, names if only string"""
-        if type(attrs) is list and all([type(a) == str for a in attrs]):
+        """Get dict representation, filter by names if only string attributes."""
+        if isinstance(attrs, list) and all(isinstance(a, str) for a in attrs):
             return {name: value for name, value in self.__dict__.items()
                     if name in attrs}
-        else:
-            return self.__dict__.copy()
+        return self.__dict__.copy()
